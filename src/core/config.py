3f346178b97a5121f348347f0c3c7bddafbfd5ba
dvntcdr <dvntcdr@gmail.com>
2026-08-09 14:07:42 +0300

    feat: add project settings config

---
+++ src/core/config.py
@@ -0,0 +1,35 @@
+from functools import lru_cache
+from typing import Any
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from src.version import __version__
+
+
+class Settings(BaseSettings):
+
+    APP_NAME: str = 'LunarGarden'
+    DESCRIPTION: str = 'The API for Plant Parents.'
+    VERSION: str = __version__
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        case_sensitive=True,
+        extra='ignore'
+    )
+
+    @property
+    def fastapi_kwargs(self) -> dict[str, Any]:
+        return {
+            'title': self.APP_NAME,
+            'description': self.DESCRIPTION,
+            'version': self.VERSION,
+        }
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
