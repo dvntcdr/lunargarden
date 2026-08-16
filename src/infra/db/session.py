8b15ba376cff92bbbffd5fe1327a495466d78279
dvntcdr <dvntcdr@gmail.com>
2026-08-16 16:35:25 +0300

    feat: add user repository dependency

--- src/infra/db/session.py
+++ src/infra/db/session.py
@@ -1,4 +1,4 @@
-from typing import AsyncGenerator
+from collections.abc import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio.engine import create_async_engine
