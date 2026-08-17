f151f2e4688aa4e2b43f06072e4574378b7bc68f
dvntcdr <dvntcdr@gmail.com>
2026-08-17 09:06:29 +0300

    feat: add custom app exceptions

---
+++ src/core/exceptions.py
@@ -0,0 +1,14 @@
+from fastapi import status
+
+
+class AppException(Exception):
+    status_code: int
+    detail: str
+
+    def __init__(self, detail: str | None = None) -> None:
+        self.detail = detail or self.__class__.detail
+
+
+class AlreadyExists(AppException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = 'Already exists'
