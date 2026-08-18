8d1c5c0b545b108bbe61da3b3b98f90dd15502d4
dvntcdr <dvntcdr@gmail.com>
2026-08-18 14:19:19 +0300

    feat: add custom exception handler

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 from src import models  # noqa
-from src.core.config import settings
 from src.api.v1.router import v1_router
+from src.core.config import settings
+from src.core.exceptions import AppException
 
 app = FastAPI(**settings.fastapi_kwargs)
 
 app.include_router(v1_router)
+
+
+@app.exception_handler(AppException)
+async def app_exception_handler(request: Request, exc: AppException) -> JSONResponse:
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={'message': exc.detail}
+    )
