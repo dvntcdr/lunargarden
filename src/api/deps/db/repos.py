1877d219abf6ecf2bb5323ac5a1630d61d628717
dvntcdr <dvntcdr@gmail.com>
2026-08-23 08:28:31 +0300

    feat: add refresh token dependency

--- src/api/deps/db/repos.py
+++ src/api/deps/db/repos.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import Depends
 
 from src.api.deps.db.session import SessionDep
+from src.repos.refresh_token import RefreshTokenRepository
 from src.repos.user import UserRepository
 
 
@@ -10,4 +11,9 @@ def get_user_repo(session: SessionDep) -> UserRepository:
 	return UserRepository(session)
 
 
+def get_token_repo(session: SessionDep) -> RefreshTokenRepository:
+	return RefreshTokenRepository(session)
+
+
 UserRepoDep = Annotated[UserRepository, Depends(get_user_repo)]
+TokenRepoDep = Annotated[RefreshTokenRepository, Depends(get_token_repo)]
