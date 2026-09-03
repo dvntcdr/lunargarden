5aad338643d52c88d4b7d0020fc875b9b12d3246
dvntcdr <dvntcdr@gmail.com>
2026-09-03 12:30:09 +0300

    feat: add plant repo dependency

--- src/api/deps/db/repos.py
+++ src/api/deps/db/repos.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import Depends
 
 from src.api.deps.db.session import SessionDep
+from src.repos.plant import PlantRepository
 from src.repos.refresh_token import RefreshTokenRepository
 from src.repos.user import UserRepository
 
@@ -15,5 +16,10 @@ def get_token_repo(session: SessionDep) -> RefreshTokenRepository:
 	return RefreshTokenRepository(session)
 
 
+def get_plant_repo(session: SessionDep) -> PlantRepository:
+	return PlantRepository(session)
+
+
 UserRepoDep = Annotated[UserRepository, Depends(get_user_repo)]
 TokenRepoDep = Annotated[RefreshTokenRepository, Depends(get_token_repo)]
+PlantRepoDep = Annotated[PlantRepository, Depends(get_plant_repo)]
